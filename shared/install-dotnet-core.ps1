1cdc14497004d7ca6ce0a0fa436e301442289034
Chatchai Daecha <chkob81@hotmail.com>
2022-02-18 13:00:24 +0700

    attempt to install with a list from variable

--- shared/install-dotnet-core.ps1
+++ shared/install-dotnet-core.ps1
@@ -1,2 +1,2 @@
 import-module .\dotnetcorecheck.psm1
-Install-DotNetCorePackages -VersionList "dotnet_core_2_1,dotnet_core_3_1_22,dotnet_core_5_0,dotnet_core_6_0"
+Install-DotNetCorePackages -VersionList ${env:DOT_NET_CORE_LIST}
